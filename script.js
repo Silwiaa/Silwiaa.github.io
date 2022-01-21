2d8e38786e123ebb6fe49fffa87157ba6c037f96
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-01-21 15:31:24 +0100

    change apiRoot

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://kodilla-tasks-sk.herokuapp.com/v1/task';
+  var apiRoot = 'https://kodilla-tasks-sk.herokuapp.com/v1/task/getTasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
