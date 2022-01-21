9417dea03dcd94acda17c5b874bdeb959325b442
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-01-21 17:05:13 +0100

    Refactor DELETE method in script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parent().parent();
     var taskId = parentEl.attr('data-task-id');
-    var requestUrl = apiRoot + 'deleteTask';
+    var requestUrl = apiRoot;
 
     $.ajax({
       url: requestUrl + $.param({
